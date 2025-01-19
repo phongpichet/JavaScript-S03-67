b3329406d0102c13c6828b696f27f477a3292e79
phongpichet <luckyked20@email.com>
2025-01-19 11:41:38 +0700

    n

--- midterm/6.js
+++ midterm/6.js
@@ -12,4 +12,3 @@ function removeDups(arr) {
   console.log(removeDups([1, 0, 1, 0])); // [1, 0]
   console.log(removeDups(["The", "big", "cat"])); // ["The", "big", "cat"]
   console.log(removeDups(["John", "Taylor", "John"])); // ["John", "Taylor"]
-  
